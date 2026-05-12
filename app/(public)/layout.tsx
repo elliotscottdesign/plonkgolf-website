b067bb71a1aa93cfe2dc89621db50b16b294b5cc
Elliot Scott <elliotscott@MacBook-Pro-9.local>
2026-05-12 10:32:56 +0100

    Revert "Preview gate: lock the public site behind code PLONK for soft launch"
    This reverts commit c81effd8fde08fbfa9b7095e5fb136d9627e5021.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import PreviewGate from "@/components/PreviewGate";
 import { ContentProvider } from "@/lib/content";
 
 export default function PublicLayout({
@@ -9,14 +8,12 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <PreviewGate>
-      <ContentProvider>
-        <div className="flex min-h-screen flex-col">
-          <Header />
-          <div className="flex-1">{children}</div>
-          <Footer />
-        </div>
-      </ContentProvider>
-    </PreviewGate>
+    <ContentProvider>
+      <div className="flex min-h-screen flex-col">
+        <Header />
+        <div className="flex-1">{children}</div>
+        <Footer />
+      </div>
+    </ContentProvider>
   );
 }
